656ea8dbb2f5b8a46397d08a7b63bc8010643f3d
yew <39639471+yewinston@users.noreply.github.com>
2020-08-04 13:18:58 -0700

    Initial commit

---
+++ reversibot.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class ReversiBot {
+    public:
+        vector<vector<int>> board;
+
+
+        ReversiBot(vector<vector<int>> board){
+            board = board;
+        }
+
+        void print_board(){
+            
+        }
+};
+
+int main(){
+    return 0;
+}
---
+++ reversibot.h
@@ -0,0 +1,8 @@
+#ifndef REVERSIBOT_H
+#define REVERSIBOT_H
+
+class ReversiBot{
+    public:
+        vector<vector<int>> board;
+        void print_board(){ return 0; }
+}
