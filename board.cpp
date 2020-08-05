d834ae6141eeec331412936597257e1cd49eb5ca
nguyenmar <66492392+nguyenmar@users.noreply.github.com>
2020-08-05 16:48:41 -0700

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -27,7 +27,7 @@ int print_board() {
     //vectors of size m.  
     vector<vector<int> > board( n , vector<int> (m));  
 
-    cout << "\n    0   1   2   3   4   5   6   7" << endl;
+    cout << "\n    A   B   C   D   E   F   G   H" << endl;
     for (int i = 0; i < n; i++) { 
         cout<<"----------------------------------- "<<endl;
         cout << "\n" << i << " ";
